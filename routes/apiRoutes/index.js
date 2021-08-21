d8478c18b1be50557017c8b7a8ccec9da21f4f38
Cooper <cooperflynn@utexas.edu>
2021-08-21 14:02:11 -0500

    added post route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,24 @@
-const { notes } = require('../../db/db');
+const { createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 const router = require('express').Router();
 
 
-router.get('/api/notes', (req, res) => {
-    let results = notes
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
-    res.json(results);
+router.post('/notes', (req, res) => {
+    //set ID with uuid NPM package
+    req.body.id = uuidv4();
+
+    // add note to json file and animals array if input is valid 
+    // if (!validateNote(req.body)) {
+    //     res.status(400).send('The note is not properly formatted.');
+    // } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    // }
 });
 
 module.exports = router;
